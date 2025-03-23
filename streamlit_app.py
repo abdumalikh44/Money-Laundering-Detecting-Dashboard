97e0938c000f885407d6c69c5e995e94286ff958
Abdu Malikh <abdumalikh44@upi.edu>
2025-03-23 22:49:10 +0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,30 +28,6 @@ new_dfs, code = spreadsheet(aml_data)
 code = code if code else "# Edit the spreadsheet above to generate code"
 st.code(code)
 
-def clear_mito_backend_cache():
-    _get_mito_backend.clear()
-
-    # Function to cache the last execution time - so we can clear periodically
-    @st.cache_resource
-    def get_cached_time():
-        # Initialize with a dictionary to store the last execution time
-        return {"last_executed_time": None}
-
-def try_clear_cache():
-
-    # How often to clear the cache
-    CLEAR_DELTA = timedelta(hours=12)
-
-    current_time = datetime.now()
-    cached_time = get_cached_time()
-
-    # Check if the current time is different from the cached last execution time
-    if cached_time["last_executed_time"] is None or cached_time["last_executed_time"] + CLEAR_DELTA < current_time:
-        clear_mito_backend_cache()
-        cached_time["last_executed_time"] = current_time
-
-try_clear_cache()
-
 st.sidebar.write("Enter transaction details to detect whether it is suspicious.")
 # Form for transaction input
 with st.sidebar.form("transaction_form"):
