0db51ced5a672740ac5aa40789363af8d1df14d1
Abdu Malikh <abdumalikh44@upi.edu>
2025-07-14 15:36:01 +0700

    update

--- pages/Visualizations.py
+++ pages/Visualizations.py
@@ -30,23 +30,20 @@ def filter_by_date(df):
     if "Date" in df.columns:
         min_date = df["Date"].min()
         max_date = df["Date"].max()
-        default_date = min_date
 
         selected_date = st.date_input(
             "📅 Filter by Transaction Date",
             min_value=min_date,
             max_value=max_date,
-            value=default_date
+            value=min_date
         )
 
-        # Filter hanya aktif jika user memilih tanggal yang berbeda dari default
-        if selected_date != default_date:
-            df = df[df["Date"] == selected_date]
-        else:
-            st.info("💡 Silakan pilih tanggal transaksi untuk menampilkan hasil.")
+        df = df[df["Date"] == selected_date]  # langsung filter tanpa kondisi
+
     else:
         st.warning("⚠️ Column 'Date' not found.")
     return df
+
 
 # ----------------- Filter by Laundering Label -----------------
 def filter_by_laundering(df):
